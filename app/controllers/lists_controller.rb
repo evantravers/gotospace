111d293d732a458a4b4625b46f86092de5398be1
Evan Travers <widgetwebworks@gmail.com>
2011-07-06 10:42:04 -0500

    it works!

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,20 +1,14 @@
 class ListsController < ApplicationController
   before_filter :authenticate_user!
+  # GET /lists
+  # GET /lists.json
   def index
-    @lists = Lists.all
-    
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @lists}
-    end
-  end
-  
-  def new
-    @list = Lists.new
+    @lists = current_user.lists
 
     respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @list }
+      format.html # index.html.erb
+      format.json { render json: @lists }
     end
   end
+
 end
