63061d58561318d66ebd61627cfd94bf44778479
Evan Travers <widgetwebworks@gmail.com>
2011-07-06 10:51:52 -0500

    it works!

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,4 +11,74 @@ class ListsController < ApplicationController
     end
   end
 
+  # GET /lists/1
+  # GET /lists/1.json
+  def show
+    @list = current_user.lists.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @list }
+    end
+  end
+
+  # GET /lists/new
+  # GET /lists/new.json
+  def new
+    @list = current_user.lists.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @list }
+    end
+  end
+
+  # GET /lists/1/edit
+  def edit
+    @list = current_user.lists.find(params[:id])
+  end
+
+  # POST /lists
+  # POST /lists.json
+  def create
+    @list = current_user.lists.new(params[:list])
+
+    respond_to do |format|
+      if @list.save
+        format.html { redirect_to @list, notice: 'List was successfully created.' }
+        format.json { render json: @list, status: :created, location: @list }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /lists/1
+  # PUT /lists/1.json
+  def update
+    @list = current_user.lists.find(params[:id])
+
+    respond_to do |format|
+      if @list.update_attributes(params[:list])
+        format.html { redirect_to @list, notice: 'List was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /lists/1
+  # DELETE /lists/1.json
+  def destroy
+    @list = current_user.lists.find(params[:id])
+    @list.destroy
+
+    respond_to do |format|
+      format.html { redirect_to lists_url }
+      format.json { head :ok }
+    end
+  end
 end
