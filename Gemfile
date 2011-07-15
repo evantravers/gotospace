be8c8c9cacdf88484dcb3628167d2c9cf4a39eaf
Tom Miller <tom.miller@luckie.com>
2011-07-15 07:55:50 -0500

    Remove meaningless code

--- Gemfile
+++ Gemfile
@@ -2,14 +2,12 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.1.0.rc4'
 gem 'sqlite3'
-gem 'devise'
 
 # Asset template engines
 gem 'sass-rails', "~> 3.1.0.rc"
 gem 'coffee-script'
 gem 'uglifier'
 gem 'jquery-rails'
-
 
 group :test, :development do
   gem 'ruby-debug19', :require => 'ruby-debug'
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,85 +1,46 @@
 class ListsController < ApplicationController
   before_filter :authenticate_user!
-  # GET /lists
-  # GET /lists.json
+
   def index
     @lists = current_user.lists
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @lists }
-    end
   end
 
-  # GET /lists/1
-  # GET /lists/1.json
   def show
     @list = current_user.lists.find(params[:id])
-    @todos = @list.todos
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @list }
-    end
   end
 
-  # GET /lists/new
-  # GET /lists/new.json
   def new
     @list = current_user.lists.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @list }
-    end
   end
 
-  # GET /lists/1/edit
   def edit
     @list = current_user.lists.find(params[:id])
   end
 
-  # POST /lists
-  # POST /lists.json
   def create
     @list = current_user.lists.new(params[:list])
 
-    respond_to do |format|
-      if @list.save
-        format.html { redirect_to @list, notice: 'List was successfully created.' }
-        format.json { render json: @list, status: :created, location: @list }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @list.errors, status: :unprocessable_entity }
-      end
+    if @list.save
+       redirect_to @list, notice: 'List was successfully created.'
+    else
+       render 'new'
     end
   end
 
-  # PUT /lists/1
-  # PUT /lists/1.json
   def update
     @list = current_user.lists.find(params[:id])
 
-    respond_to do |format|
-      if @list.update_attributes(params[:list])
-        format.html { redirect_to @list, notice: 'List was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @list.errors, status: :unprocessable_entity }
-      end
+    if @list.update_attributes(params[:list])
+     redirect_to @list, notice: 'List was successfully updated.'
+    else
+     render 'edit'
     end
   end
 
-  # DELETE /lists/1
-  # DELETE /lists/1.json
   def destroy
     @list = current_user.lists.find(params[:id])
     @list.destroy
 
-    respond_to do |format|
-      format.html { redirect_to lists_url }
-      format.json { head :ok }
-    end
+    redirect_to [:lists]
   end
 end
