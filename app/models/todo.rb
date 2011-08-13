5f7b7f2f24fa90ff4ef1151b3f6dff9fd137e712
Evan Travers <widgetwebworks@gmail.com>
2011-08-13 14:17:06 -0500

    Updated look, removed unneeded elements from todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,4 +2,10 @@ class Todo < ActiveRecord::Base
   belongs_to :list
   belongs_to :todo
   has_many :todos
+
+  after_initialize :set_default
+
+  def set_default
+    self.completed = false if self.completed == nil
+  end
 end
